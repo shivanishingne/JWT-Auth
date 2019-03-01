28b10e1f8d30713a92e04a6b6ffa5cec189d4e85
interns2019 <shivanishingne@gmail.com>
2019-03-01 22:13:39 +0530

    Controller

---
+++ src/main/java/com/jwtme/controllers/UserController.java
@@ -0,0 +1,16 @@
+package com.jwtme.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	@RequestMapping("/users")
+	@ResponseBody
+	public String getUsers() {
+		return "{\"users\":[{\"name\":\"Lucas\", \"country\":\"Brazil\"}," +
+		           "{\"name\":\"Jackie\",\"country\":\"China\"}]}";
+	}
+}
